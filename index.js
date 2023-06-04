9eb255fc0687d0ff0b93402aebb652e6cd51e854
kNelakonda <knelakon@ucsd.edu>
2023-06-04 01:29:58 -0700

    moved script to new file

---
+++ index.js
@@ -0,0 +1,13 @@
+    let form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      let output = document.querySelector('output');
+      let firstNum = document.querySelector('#first-num').value;
+      let secondNum = document.querySelector('#second-num').value;
+      let operator = document.querySelector('#operator').value;
+      output.innerHTML = eval(`${firstNum} ${operator} ${secondNum}`);
+    });
+
+    let errorBtns = Array.from(document.querySelectorAll('#error-btns > button'));
+
+
